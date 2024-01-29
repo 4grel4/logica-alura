f3de8cfb198af67f4b939000d0b326ee863492b2
4grel4 <gabriel_agrela2020@hotmail.com>
2024-01-29 13:00:21 -0300

    mudando de Arrasou para Parabens

--- jogo-numero-secreto/app.js
+++ jogo-numero-secreto/app.js
@@ -50,7 +50,7 @@ const verificarChute = () => {
     let chute = document.querySelector('input').value;
 
     if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Arrasou!');
+        exibirTextoNaTela('h1', 'Parabéns!');
     let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
     let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
